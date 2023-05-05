3bd3229a1e53c38657d2d25b77e2b556639ede96
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-05-05 09:30:12 +0100

    Initial Commit

---
+++ src/main/java/uk/gov/laa/caab/CaabApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.laa.caab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CaabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaabApplication.class, args);
+	}
+
+}
