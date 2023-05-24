5f043120b24b1caea5a43e86604aef04ee2ebdf8
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-05-24 09:30:55 +0100

    Add basic saml authentication
    * Add basic saml authentication
    * update readme with saml mock setup

---
+++ src/main/java/uk/gov/laa/caab/advice/SamlPrincipalControllerAdvice.java
@@ -0,0 +1,18 @@
+package uk.gov.laa.caab.advice;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class SamlPrincipalControllerAdvice {
+
+    @ModelAttribute
+    public void addSamlPrincipalToModel(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+        if (principal != null) {
+            model.addAttribute("userAttributes", principal.getAttributes());
+        }
+    }
+}
