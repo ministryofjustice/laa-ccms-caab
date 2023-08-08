fd695c298d7e254da5339c43ddf5afd6287479b6
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2023-08-08 14:12:54 +0100

    Remove category description, generalise the function

--- src/main/resources/static/assets/js/updateHiddenField.js
+++ src/main/resources/static/assets/js/updateHiddenField.js
@@ -1,6 +1,5 @@
 function updateHiddenField(selectElementId, hiddenFieldId) {
     var selectElement = document.getElementById(selectElementId);
     var selectedOption = selectElement.options[selectElement.selectedIndex];
-    var categoryDescription = selectedOption.text;
-    document.getElementById(hiddenFieldId).value = categoryDescription;
+    document.getElementById(hiddenFieldId).value = selectedOption.text;
 }
