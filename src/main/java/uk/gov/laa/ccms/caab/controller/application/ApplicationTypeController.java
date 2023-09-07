ea3d876ac14bbcbfa9b675b5f8cb0c3133f91717
Andrew Porritt <109674546+porritta@users.noreply.github.com>
2023-09-07 12:41:56 +0100

    refactor(ccls-1771): checkstyle

--- src/main/java/uk/gov/laa/ccms/caab/controller/application/ApplicationTypeController.java
+++ src/main/java/uk/gov/laa/ccms/caab/controller/application/ApplicationTypeController.java
@@ -41,7 +41,7 @@ public class ApplicationTypeController {
    * @param applicationDetails The application details from session.
    * @param model              The model for the view.
    * @return The view name for the application type selection page or a redirect if exceptional
-   * funding.
+   *     funding.
    */
   @GetMapping("/application/application-type")
   public String applicationType(
