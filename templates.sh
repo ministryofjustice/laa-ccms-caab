04ac4b9e865269bd3b2b5c4f9562125be5de96df
keith-h <keith.humphreys@digital.justice.gov.uk>
2023-09-28 12:28:41 +0100

    feat(ccls-1829): moj-frontend

--- templates.sh
+++ templates.sh
@@ -15,3 +15,17 @@ mv src/main/resources/static/govuk-frontend-4.6.0.min.js src/main/resources/stat
 rm -rf src/main/resources/static/govuk-frontend-*
 rm -rf govuk_frontend.zip
 rm -rf src/main/resources/static/VERSION.txt
+
+#TODO UPDATE FOR MOJ FRONTEND
+curl -L https://github.com/ministryofjustice/moj-frontend/releases/download/v1.8.0/release-v1.8.0.zip > moj_frontend.zip
+unzip -o moj_frontend.zip -d src/main/resources/static
+mkdir src/main/resources/static/moj-frontend
+mv src/main/resources/static/moj-frontend.min.css src/main/resources/static/moj-frontend/moj-frontend.min.css
+rm -rf src/main/resources/static/moj-frontend-ie8.min.css
+mv src/main/resources/static/moj-frontend.min.js src/main/resources/static/moj-frontend/javascript/moj-frontend.min.js
+mv src/main/resources/static/assets/* src/main/resources/static/assets/
+
+# Tidy up
+rm -rf moj_frontend.zip
+
+
