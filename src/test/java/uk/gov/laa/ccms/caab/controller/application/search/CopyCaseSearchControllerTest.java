cde3e0c8239765580fc2df9ae74490b190b5e345
Andrew Porritt <109674546+porritta@users.noreply.github.com>
2024-01-11 12:38:33 +0000

    story(ccls-1708): combine search and results controllers

--- src/test/java/uk/gov/laa/ccms/caab/controller/application/search/CopyCaseSearchControllerTest.java
+++ src/test/java/uk/gov/laa/ccms/caab/controller/application/search/CopyCaseSearchControllerTest.java
@@ -2,9 +2,13 @@ package uk.gov.laa.ccms.caab.controller.application.search;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,9 +18,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_FORM_DATA;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.CASE_SEARCH_CRITERIA;
+import static uk.gov.laa.ccms.caab.constants.SessionConstants.CASE_SEARCH_RESULTS;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.USER_DETAILS;
 
+import jakarta.servlet.ServletException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,13 +39,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.Errors;
 import org.springframework.web.context.WebApplicationContext;
 import reactor.core.publisher.Mono;
+import uk.gov.laa.ccms.caab.bean.ApplicationFormData;
 import uk.gov.laa.ccms.caab.bean.CaseSearchCriteria;
 import uk.gov.laa.ccms.caab.bean.validators.application.CaseSearchCriteriaValidator;
 import uk.gov.laa.ccms.caab.constants.SearchConstants;
 import uk.gov.laa.ccms.caab.exception.CaabApplicationException;
+import uk.gov.laa.ccms.caab.exception.TooManyResultsException;
+import uk.gov.laa.ccms.caab.mapper.ApplicationMapper;
+import uk.gov.laa.ccms.caab.model.ApplicationDetails;
+import uk.gov.laa.ccms.caab.model.BaseApplication;
+import uk.gov.laa.ccms.caab.service.ApplicationService;
 import uk.gov.laa.ccms.caab.service.ProviderService;
 import uk.gov.laa.ccms.data.model.BaseOffice;
 import uk.gov.laa.ccms.data.model.BaseProvider;
+import uk.gov.laa.ccms.data.model.CaseStatusLookupValueDetail;
 import uk.gov.laa.ccms.data.model.ContactDetail;
 import uk.gov.laa.ccms.data.model.ProviderDetail;
 import uk.gov.laa.ccms.data.model.UserDetail;
@@ -54,6 +68,12 @@ public class CopyCaseSearchControllerTest {
   private ProviderService providerService;
 
   @Mock
+  private ApplicationService applicationService;
+
+  @Mock
+  private ApplicationMapper applicationMapper;
+
+  @Mock
   private SearchConstants searchConstants;
 
   @InjectMocks
@@ -61,12 +81,15 @@ public class CopyCaseSearchControllerTest {
 
   private MockMvc mockMvc;
 
+  private UserDetail user;
+
   @Autowired
   private WebApplicationContext webApplicationContext;
 
   @BeforeEach
   public void setup() {
     mockMvc = standaloneSetup(copyCaseSearchController).build();
+    this.user = buildUser();
 
     when(searchConstants.getMaxSearchResultsCases()).thenReturn(200);
   }
@@ -143,6 +166,105 @@ public class CopyCaseSearchControllerTest {
         .andDo(print())
         .andExpect(status().is3xxRedirection())
         .andExpect(redirectedUrl("/application/copy-case/results"));
+  }
+
+  @Test
+  public void testGetCopyCaseSearchResults_NoCopyCaseStatus() throws Exception {
+    List<BaseApplication> baseApplications = new ArrayList<>();
+
+    when(applicationService.getCases(any(), any(), any())).thenReturn(baseApplications);
+
+    CaseSearchCriteria caseSearchCriteria = new CaseSearchCriteria();
+    this.mockMvc.perform(get("/application/copy-case/results")
+            .sessionAttr(USER_DETAILS, user)
+            .sessionAttr(CASE_SEARCH_CRITERIA, caseSearchCriteria))
+        .andExpect(status().isOk())
+        .andExpect(view().name("application/application-copy-case-search-no-results"));
+
+    verify(applicationService).getCopyCaseStatus();
+    verify(applicationService).getCases(eq(caseSearchCriteria), any(), any());
+    assertNull(caseSearchCriteria.getStatus());
+  }
+
+  @Test
+  public void testGetCopyCaseSearchResults_NoResults() throws Exception {
+    List<BaseApplication> baseApplications = new ArrayList<>();
+
+    when(applicationService.getCases(any(), any(), any())).thenReturn(baseApplications);
+
+    String COPY_STATUS_CODE = "APP";
+    when(applicationService.getCopyCaseStatus()).thenReturn(
+        new CaseStatusLookupValueDetail().code(COPY_STATUS_CODE));
+
+    CaseSearchCriteria caseSearchCriteria = new CaseSearchCriteria();
+    this.mockMvc.perform(get("/application/copy-case/results")
+            .sessionAttr(USER_DETAILS, user)
+            .sessionAttr(CASE_SEARCH_CRITERIA, caseSearchCriteria))
+        .andExpect(status().isOk())
+        .andExpect(view().name("application/application-copy-case-search-no-results"));
+
+    verify(applicationService).getCopyCaseStatus();
+    verify(applicationService).getCases(eq(caseSearchCriteria), any(), any());
+    assertEquals(COPY_STATUS_CODE, caseSearchCriteria.getStatus());
+  }
+
+  @Test
+  public void testGetCopyCaseSearchResults_WithTooManyResults() throws Exception {
+
+    when(applicationService.getCases(any(), any(), any())).thenThrow(
+        new TooManyResultsException(""));
+
+    this.mockMvc.perform(get("/application/copy-case/results")
+            .sessionAttr(USER_DETAILS, user)
+            .sessionAttr(CASE_SEARCH_CRITERIA, new CaseSearchCriteria()))
+        .andExpect(status().isOk())
+        .andExpect(view().name(
+            "application/application-copy-case-search-too-many-results"));
+  }
+
+  @Test
+  public void testGetCopyCaseSearchResults_WithResults() throws Exception {
+    List<BaseApplication> baseApplications = List.of(new BaseApplication());
+
+    when(applicationService.getCases(any(), any(), any())).thenReturn(baseApplications);
+
+    this.mockMvc.perform(get("/application/copy-case/results")
+            .sessionAttr(USER_DETAILS, user)
+            .sessionAttr(CASE_SEARCH_CRITERIA, new CaseSearchCriteria()))
+        .andExpect(status().isOk())
+        .andExpect(view().name("application/application-copy-case-search-results"));
+  }
+
+  @Test
+  public void testSelectCopyCaseReferenceNumber_InvalidCaseRef() {
+    Exception exception = assertThrows(ServletException.class, () ->
+        this.mockMvc.perform(get("/application/copy-case/{caseRef}/confirm", "123")
+            .sessionAttr(CASE_SEARCH_RESULTS, new ApplicationDetails())
+            .sessionAttr(APPLICATION_FORM_DATA, new ApplicationFormData())));
+
+    assertInstanceOf(CaabApplicationException.class, exception.getCause());
+
+    String expectedMessage = "Invalid copyCaseReferenceNumber supplied";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+  }
+
+  @Test
+  public void testSelectCopyCaseReferenceNumber_ValidCaseRef() throws Exception {
+    ApplicationDetails applicationDetails = new ApplicationDetails()
+        .addContentItem(new BaseApplication().caseReferenceNumber("123"))
+        .size(1)
+        .totalElements(1);
+
+    ApplicationFormData applicationFormData = new ApplicationFormData();
+    this.mockMvc.perform(get("/application/copy-case/{caseRef}/confirm", "123")
+            .sessionAttr(CASE_SEARCH_RESULTS, applicationDetails)
+            .sessionAttr(APPLICATION_FORM_DATA, applicationFormData))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/application/client/search"));
+
+    assertEquals("123", applicationFormData.getCopyCaseReferenceNumber());
   }
 
   private UserDetail buildUser() {
