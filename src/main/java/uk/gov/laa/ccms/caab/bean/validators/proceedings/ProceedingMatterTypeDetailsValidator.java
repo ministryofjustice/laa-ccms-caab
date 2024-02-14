474fdf0c3f0c7ab85fa960a3304dc888ede11906
Phil Payne <126681069+PhilDigitalJustice@users.noreply.github.com>
2024-02-14 11:22:24 +0000

    Story/ccls 1975 proceedings and costs - proceeding details screens (#138)
    * CCLS-1975
    
    includes:
    
    - CCLS-1990 proceeding details overview screen
    - CCLS-1991 proceeding step screens x4
    
    * PR amendments

---
+++ src/main/java/uk/gov/laa/ccms/caab/bean/validators/proceedings/ProceedingMatterTypeDetailsValidator.java
@@ -0,0 +1,44 @@
+package uk.gov.laa.ccms.caab.bean.validators.proceedings;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import uk.gov.laa.ccms.caab.bean.ClientFormDataMonitoringDetails;
+import uk.gov.laa.ccms.caab.bean.proceeding.ProceedingFormDataMatterTypeDetails;
+import uk.gov.laa.ccms.caab.bean.validators.AbstractValidator;
+
+/**
+ * Validator the matter type details provided by proceeding flow forms.
+ */
+@Component
+public class ProceedingMatterTypeDetailsValidator extends AbstractValidator {
+
+  /**
+   * Determines if the Validator supports the provided class.
+   *
+   * @param clazz The class to check for support.
+   * @return {@code true} if the class is assignable from
+   *         {@link uk.gov.laa.ccms.caab.bean.proceeding.ProceedingFormDataMatterTypeDetails},
+   *         {@code false} otherwise.
+   */
+  @Override
+  public boolean supports(final Class<?> clazz) {
+    return ProceedingFormDataMatterTypeDetails.class.isAssignableFrom(clazz);
+  }
+
+  /**
+   * Validates the matter type details in the
+   * {@link uk.gov.laa.ccms.caab.bean.proceeding.ProceedingFormDataMatterTypeDetails}.
+   *
+   * @param target The object to be validated.
+   * @param errors The Errors object to store validation errors.
+   */
+  @Override
+  public void validate(final Object target, final Errors errors) {
+    final ProceedingFormDataMatterTypeDetails matterTypeDetails =
+        (ProceedingFormDataMatterTypeDetails) target;
+
+    validateRequiredField("matterType", matterTypeDetails.getMatterType(),
+        "Matter type", errors);
+  }
+
+}
