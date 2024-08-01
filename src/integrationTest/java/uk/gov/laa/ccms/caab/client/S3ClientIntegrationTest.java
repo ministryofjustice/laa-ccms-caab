f052c2f50fdad2dc955d806e00de9eaa8495b806
farrell-m <michael.farrell@digital.justice.gov.uk>
2024-08-01 12:03:41 +0100

    include security configuration

--- src/integrationTest/java/uk/gov/laa/ccms/caab/client/S3ClientIntegrationTest.java
+++ src/integrationTest/java/uk/gov/laa/ccms/caab/client/S3ClientIntegrationTest.java
@@ -18,9 +18,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.laa.ccms.caab.AbstractIntegrationTest;
 
 @SpringBootTest
-public class S3ClientIntegrationTest {
+public class S3ClientIntegrationTest extends AbstractIntegrationTest {
 
   @Autowired
   private S3ApiClient s3ApiClient;
