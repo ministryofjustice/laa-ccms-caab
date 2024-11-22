38f756b0f6ba0a750498905dda69d18831f214c3
Jamie Briggs <jamie.briggs@digital.justice.gov.uk>
2024-11-22 15:55:16 +0000

    Remove core.scss from ccms.scss and created assessment-get.scss as it's used for one template and conflicts with GOVUK frontend
    Signed-off-by: Jamie Briggs <jamie.briggs@digital.justice.gov.uk>

--- gulpfile.js
+++ gulpfile.js
@@ -78,6 +78,18 @@ function compileCCMSStyleSheets(){
   .pipe(gulp.dest('./src/main/resources/static/ccms/'));
 }
 
+function compileCCMSAssessmentStyleSheets(){
+  return gulp.src('./src/main/resources/scss/assessment-get.scss')
+  // Will highlight errors in your CSS file incase they are missed
+  .pipe(plumber())
+  .pipe(sass({
+    outputStyle: "compressed"
+  }))
+  .pipe(plumber.stop())
+  .pipe(autoprefixer({cascade: false})) // Adds up to date vendor prefixes
+  .pipe(gulp.dest('./src/main/resources/static/ccms/'));
+}
+
 // GOV UK Copy Task called "copyGOVUKAssets"
 gulp.task('copyGOVUKAssets',
     parallel(copyGOVUKStyleSheets, copyGOVUKJavaScript, copyGOVUKAssets));
@@ -85,7 +97,7 @@ gulp.task('copyGOVUKAssets',
 gulp.task('copyMOJAssets', parallel(copyMOJStyleSheets, copyMOJJavaScript, copyMOJAssets));
 
 // CCMS Task called "compileCCMSAssets"
-gulp.task('compileCCMSAssets', compileCCMSStyleSheets);
+gulp.task('compileCCMSAssets', parallel(compileCCMSStyleSheets, compileCCMSAssessmentStyleSheets));
 
 // As a default task, it should just run all other tasks, so defined as a series
 //  by each other tasks name.
