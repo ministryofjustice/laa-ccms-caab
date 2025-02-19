53fb3b0e44bb814856dd8949c313b2512af4b509
farrell-m <michael.farrell@digital.justice.gov.uk>
2025-02-19 15:19:00 +0000

    checkstyle

--- src/main/java/uk/gov/laa/ccms/caab/advice/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/laa/ccms/caab/advice/GlobalExceptionHandler.java
@@ -23,11 +23,13 @@ public class GlobalExceptionHandler {
    * error response.
    *
    * @param e The EbsApiClientException that was thrown.
+   * @param session The HttpSession for the current request.
    * @param model The Model object to which error information will be added.
    * @return The name of the error view to be displayed.
    */
   @ExceptionHandler(value = {EbsApiClientException.class})
-  public String handleDataApiClientException(EbsApiClientException e,  HttpSession session, Model model) {
+  public String handleDataApiClientException(EbsApiClientException e,
+      HttpSession session, Model model) {
     // This exception is thrown when there's a low-level, resource-specific error,
     // such as an I/O error, Log the error details
     return generalErrorView(model, session, e);
@@ -38,11 +40,13 @@ public class GlobalExceptionHandler {
    * appropriate error response.
    *
    * @param e The CaabApplicationException that was thrown.
+   * @param session The HttpSession for the current request.
    * @param model The Model object to which error information will be added.
    * @return The name of the error view to be displayed.
    */
   @ExceptionHandler(value = {CaabApplicationException.class})
-  public String handleCaabApplicationException(CaabApplicationException e,  HttpSession session, Model model) {
+  public String handleCaabApplicationException(CaabApplicationException e,
+      HttpSession session, Model model) {
     return generalErrorView(model, session, e);
   }
 
@@ -51,6 +55,7 @@ public class GlobalExceptionHandler {
    * appropriate error response.
    *
    * @param e The ServletRequestBindingException that was thrown.
+   * @param session The HttpSession for the current request.
    * @param model The Model object to which error information will be added.
    * @return The name of the error view to be displayed.
    */
