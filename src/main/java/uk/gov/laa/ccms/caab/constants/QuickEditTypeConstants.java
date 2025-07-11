89641a7138cb5fe0dc610e649500df48e46b7b92
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-07-11 14:34:42 +0100

    CCMSPUI-527 | Checkstyle issues resolved and spotless applied
    Signed-off-by: Jamie Briggs <jamie.briggs@justice.gov.uk>

--- src/main/java/uk/gov/laa/ccms/caab/constants/QuickEditTypeConstants.java
+++ src/main/java/uk/gov/laa/ccms/caab/constants/QuickEditTypeConstants.java
@@ -1,29 +1,24 @@
 package uk.gov.laa.ccms.caab.constants;
 
+/**
+ * Constants for quick edit types when submitting an amendment to a case.
+ *
+ * @author Jamie Briggs
+ */
 public class QuickEditTypeConstants {
 
-  /**
-   * The Message Type to be used for Edit Provider amendments.
-   */
+  /** The Message Type to be used for Edit Provider amendments. */
   public static final String MESSAGE_TYPE_EDIT_PROVIDER = "ProviderDetails";
 
-  /**
-   * The Message Type to be used for Allocate cost Limits amendments.
-   */
+  /** The Message Type to be used for Allocate cost Limits amendments. */
   public static final String MESSAGE_TYPE_ALLOCATE_COST_LIMIT = "CostAlloc";
 
-  /**
-   * The Message Type to be used for Correspondence Details amendments.
-   */
+  /** The Message Type to be used for Correspondence Details amendments. */
   public static final String MESSAGE_TYPE_CASE_CORRESPONDENCE_PREFERENCE = "CorrespondenceDtls";
 
-  /**
-   * The Message Type to be used for Means reassessments amendments.
-   */
+  /** The Message Type to be used for Means reassessments amendments. */
   public static final String MESSAGE_TYPE_MEANS_REASSESSMENT = "MeansReassessment";
 
-  /**
-   * The Message Type to be used for Means Assessments Legal Amendment amendments.
-   */
+  /** The Message Type to be used for Means Assessments Legal Amendment amendments. */
   public static final String MESSAGE_TYPE_MEANS_ASSESSMENT_LEGAL_AMENDMENT = "LegalAmendment";
 }
