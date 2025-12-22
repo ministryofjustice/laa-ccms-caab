be6ed831f516fb80984a122b249600c15ee1f916
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-12-22 19:29:48 +0000

    added validation for max amount and below granted amount

--- src/main/java/uk/gov/laa/ccms/caab/controller/application/AllocateCostLimitController.java
+++ src/main/java/uk/gov/laa/ccms/caab/controller/application/AllocateCostLimitController.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import uk.gov.laa.ccms.caab.bean.costs.CostsFormData;
+import uk.gov.laa.ccms.caab.bean.validators.costs.AllocateCostLimitValidator;
 import uk.gov.laa.ccms.caab.mapper.ProceedingAndCostsMapper;
 import uk.gov.laa.ccms.caab.model.ApplicationDetail;
 import uk.gov.laa.ccms.caab.model.CostEntryDetail;
@@ -24,6 +26,7 @@ import uk.gov.laa.ccms.caab.model.CostEntryDetail;
 @Slf4j
 public class AllocateCostLimitController {
   private final ProceedingAndCostsMapper proceedingAndCostsMapper;
+  private final AllocateCostLimitValidator allocateCostLimitValidator;
 
   /**
    * Displays the cost limitation allocation screen.
@@ -53,11 +56,10 @@ public class AllocateCostLimitController {
    */
   @PostMapping("/allocate-cost-limit")
   public String updateCost(
-      @ModelAttribute("costDetails") final CostsFormData costsFormData,
+      @ModelAttribute("costDetails") CostsFormData costsFormData,
       @SessionAttribute(CASE) final ApplicationDetail ebsCase,
-      final Model model) {
-
-    final CostsFormData newFormData = proceedingAndCostsMapper.toCostsForm(ebsCase);
+      final Model model,
+      final BindingResult bindingResult) {
 
     List<CostEntryDetail> costs =
         ebsCase.getCosts().getCostEntries().stream()
@@ -70,11 +72,13 @@ public class AllocateCostLimitController {
     for (int i = 0; i < costs.size(); i++) {
       costs.get(i).setRequestedCosts(costsFormData.getCostEntries().get(i).getRequestedCosts());
     }
-    newFormData.setCostEntries(costs);
+    costsFormData.setCostEntries(costs);
+
+    allocateCostLimitValidator.validate(costsFormData, bindingResult);
 
     model.addAttribute("case", ebsCase);
-    model.addAttribute("costDetails", newFormData);
-    model.addAttribute("totalRemaining", getTotalRemaining(newFormData));
+    model.addAttribute("costDetails", costsFormData);
+    model.addAttribute("totalRemaining", getTotalRemaining(costsFormData));
 
     return "application/cost-allocation";
   }
