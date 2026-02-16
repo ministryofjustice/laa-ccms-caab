990a4a031df12cc872b820530e0da7bdc351596a
farrell-m <michael.farrell@justice.gov.uk>
2026-02-16 14:36:50 +0000

    update autoconfigure exclude

--- src/integrationTest/java/uk/gov/laa/ccms/caab/AbstractIntegrationTest.java
+++ src/integrationTest/java/uk/gov/laa/ccms/caab/AbstractIntegrationTest.java
@@ -1,13 +1,14 @@
 package uk.gov.laa.ccms.caab;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.security.saml2.autoconfigure.Saml2RelyingPartyAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.laa.ccms.caab.bean.metric.PuiMetricService;
 import uk.gov.laa.ccms.caab.config.SecurityConfiguration;
 
 @SpringBootTest
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {Saml2RelyingPartyAutoConfiguration.class})
 public abstract class AbstractIntegrationTest {
 
   @MockitoBean private SecurityConfiguration securityConfiguration;
