2b42c32a6d91191e23f6e1b62a62b4e1192e23b1
maxmak1902 <maxwell.mak@justice.gov.uk>
2026-03-12 16:05:15 +0000

    Added validation to document uploads for modernised pui and updated tests

--- src/main/java/uk/gov/laa/ccms/caab/bean/validators/file/FileUploadValidator.java
+++ src/main/java/uk/gov/laa/ccms/caab/bean/validators/file/FileUploadValidator.java
@@ -4,9 +4,12 @@ import static uk.gov.laa.ccms.caab.util.DisplayUtil.getCommaDelimitedString;
 import static uk.gov.laa.ccms.caab.util.FileUtil.getFileExtension;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.tika.Tika;
+import org.springframework.util.StringUtils;
 import org.springframework.util.unit.DataSize;
 import org.springframework.validation.Errors;
 import uk.gov.laa.ccms.caab.bean.file.FileUploadFormData;
@@ -23,11 +26,25 @@ public abstract class FileUploadValidator extends AbstractValidator {
 
   protected static final String FILE_REQUIRED_ERROR_CODE = "validation.error.selectAFile";
 
+  protected static final String INVALID_FILE_NAME_ERROR_CODE = "validation.error.invalidFileName";
+
   /** The error message for an invalid file extension. */
   protected static final String INVALID_EXTENSION_ERROR =
       "Invalid file extension. We can only accept %s files";
 
+  /** The error message for an invalid mime type. */
+  protected static final String INVALID_MIME_TYPE_ERROR =
+      "Invalid mime type. We can only accept %s mime types";
+
   protected static final String INVALID_EXTENSION_ERROR_CODE = "validation.error.invalidExtension";
+
+  protected static final String INVALID_MIME_TYPE_ERROR_CODE = "validation.error.invalidMimeType";
+
+  protected static final String INVALID_MAGIC_BYTES_ERROR_CODE =
+      "validation.error.invalidMagicBytes";
+
+  protected static final String MULTIPLE_EXTENSION_ERROR_CODE =
+      "validation.error.multipleExtension";
 
   /** The error message for an invalid file extension. */
   public static final String MAX_FILESIZE_ERROR =
@@ -44,6 +61,11 @@ public abstract class FileUploadValidator extends AbstractValidator {
   /** The configurable maximum allowed file size. */
   private final String maxFileSize;
 
+  /** The configurable list of mime types. */
+  private final List<String> validMimeTypes;
+
+  private final Tika tika = new Tika();
+
   /**
    * Validate generic file upload details such as file data, extension, type, description and size.
    *
@@ -57,6 +79,14 @@ public abstract class FileUploadValidator extends AbstractValidator {
     } else {
       fileUploadFormData.setFileExtension(getFileExtension(fileUploadFormData.getFile()));
 
+      if (!hasValidFileName(fileUploadFormData)) {
+        errors.rejectValue("file", INVALID_FILE_NAME_ERROR_CODE);
+      }
+
+      if (!hasSingleExtension(fileUploadFormData)) {
+        errors.rejectValue("file", MULTIPLE_EXTENSION_ERROR_CODE);
+      }
+
       if (!isValidExtension(fileUploadFormData.getFileExtension())) {
         errors.rejectValue(
             "file",
@@ -65,6 +95,18 @@ public abstract class FileUploadValidator extends AbstractValidator {
             INVALID_EXTENSION_ERROR.formatted(getCommaDelimitedString(validExtensions)));
       } else {
         validateFileSize(fileUploadFormData, errors);
+
+        if (!isValidMimeType(fileUploadFormData.getFile().getContentType())) {
+          errors.rejectValue(
+              "file",
+              INVALID_MIME_TYPE_ERROR_CODE,
+              new String[] {getCommaDelimitedString(validMimeTypes)},
+              INVALID_MIME_TYPE_ERROR.formatted(getCommaDelimitedString(validMimeTypes)));
+        } else {
+          if (!isValidMagicBytes(fileUploadFormData)) {
+            errors.rejectValue("file", INVALID_MAGIC_BYTES_ERROR_CODE);
+          }
+        }
       }
     }
   }
@@ -137,5 +179,58 @@ public abstract class FileUploadValidator extends AbstractValidator {
    */
   protected boolean isValidExtension(final String fileExtension) {
     return validExtensions.stream().anyMatch(ext -> ext.equalsIgnoreCase(fileExtension));
+  }
+
+  /**
+   * Check whether mime type is valid based on the allowed mime types configured by the application.
+   *
+   * @param mimeType content type of the file.
+   * @return true if the mime type is valid, false otherwise.
+   */
+  protected boolean isValidMimeType(final String mimeType) {
+    return validMimeTypes.stream().anyMatch(type -> type.equalsIgnoreCase(mimeType));
+  }
+
+  /**
+   * Check whether a file is valid by ensuring it does not have multiple extensions.
+   *
+   * @param fileUploadFormData the file upload form data object.
+   * @return true if the file only has a single extension, false otherwise.
+   */
+  protected boolean hasSingleExtension(FileUploadFormData fileUploadFormData) {
+    String filename = StringUtils.cleanPath(fileUploadFormData.getFile().getOriginalFilename());
+
+    int lastDot = filename.lastIndexOf('.');
+    int firstDot = filename.indexOf('.');
+
+    return (firstDot > 0) && (firstDot == lastDot) && lastDot < (filename.length() - 1);
+  }
+
+  /**
+   * Check whether a file has a valid filename using a regex expression.
+   *
+   * @param fileUploadFormData the file upload form data object.
+   * @return true if the filename is valid, false otherwise.
+   */
+  protected boolean hasValidFileName(FileUploadFormData fileUploadFormData) {
+    String filename = StringUtils.cleanPath(fileUploadFormData.getFile().getOriginalFilename());
+
+    return filename.matches("^[A-Za-z0-9_-]+\\.[A-Za-z0-9]+$");
+  }
+
+  /**
+   * Checks whether the file content matches the file type.
+   *
+   * @param fileUploadFormData the file upload form data object.
+   * @return true if the file content matches the file type, false otherwise.
+   */
+  protected boolean isValidMagicBytes(FileUploadFormData fileUploadFormData) {
+    try (InputStream inputStream = fileUploadFormData.getFile().getInputStream()) {
+      String detectedMime = tika.detect(inputStream);
+
+      return validMimeTypes.stream().anyMatch(type -> type.equalsIgnoreCase(detectedMime));
+    } catch (IOException e) {
+      throw new CaabApplicationException("Failed to read file data", e);
+    }
   }
 }
