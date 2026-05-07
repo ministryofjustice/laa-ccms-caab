292ffe0bd751b2056cf3eb2cfd02b0bd02e1d340
farrell-m <michael.farrell@justice.gov.uk>
2026-05-07 11:23:12 +0100

    remove import

--- src/test/java/uk/gov/laa/ccms/caab/controller/requests/ProviderRequestsControllerTest.java
+++ src/test/java/uk/gov/laa/ccms/caab/controller/requests/ProviderRequestsControllerTest.java
@@ -34,7 +34,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.ui.ExtendedModelMap;
