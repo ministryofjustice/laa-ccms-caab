639aa96fc03ee1925ce675dd66970ae08f4386c1
maxmak1902 <maxwell.mak@justice.gov.uk>
2026-06-22 14:44:21 +0100

    Amend case submission step 2
    Updates
    
    Updates
    
    updates

--- src/main/java/uk/gov/laa/ccms/caab/controller/application/AmendCaseController.java
+++ src/main/java/uk/gov/laa/ccms/caab/controller/application/AmendCaseController.java
@@ -8,6 +8,8 @@ import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_FORM_D
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_ID;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_SUMMARY;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.CASE;
+import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_RESULT;
+import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_TRANSACTION_ID;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.EVIDENCE_REQUIRED;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_RESULT;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_TRANSACTION_ID;
