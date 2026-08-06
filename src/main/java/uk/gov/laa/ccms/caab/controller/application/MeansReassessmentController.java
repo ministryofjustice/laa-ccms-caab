688661b2ccb20aba937641660844e06e3aac19b5
maxmak1902 <maxwell.mak@justice.gov.uk>
2026-08-06 14:45:34 +0100

    pr comments

--- src/main/java/uk/gov/laa/ccms/caab/controller/application/MeansReassessmentController.java
+++ src/main/java/uk/gov/laa/ccms/caab/controller/application/MeansReassessmentController.java
@@ -9,6 +9,7 @@ import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_ID;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.APPLICATION_SUMMARY;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.CASE;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_QUICK_EDIT_TYPE;
+import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_RESULT;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.SUBMISSION_TRANSACTION_ID;
 import static uk.gov.laa.ccms.caab.constants.SessionConstants.USER_DETAILS;
 import static uk.gov.laa.ccms.caab.constants.SubmissionConstants.SUBMISSION_SUBMIT_CASE;
