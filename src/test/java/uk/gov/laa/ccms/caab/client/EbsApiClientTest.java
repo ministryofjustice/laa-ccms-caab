289263e8c8ebd165128c365aa564805d66e6255e
Oluwawunmi <16096113+Oluwawunmi@users.noreply.github.com>
2026-09-09 12:24:15 +0100

    fix(CCMSPUI-698): send the bill type when looking up declarations (#848)
    * fix(CCMSPUI-698): send the bill type when looking up declarations
    
    * fix(CCMSPUI-698): treat a blank bill type as no bill type
    
    * fix(CCMSPUI-698): leave a blank declaration type off the request too

--- src/test/java/uk/gov/laa/ccms/caab/client/EbsApiClientTest.java
+++ src/test/java/uk/gov/laa/ccms/caab/client/EbsApiClientTest.java
@@ -1492,9 +1492,15 @@ public class EbsApiClientTest {
 
     @ParameterizedTest
     @CsvSource({
-      "DECLARATION_TYPE, BILL_TYPE, /lookup/declarations?size=1000&type=DECLARATION_TYPE&billType=BILL_TYPE",
+      "DECLARATION_TYPE, BILL_TYPE, /lookup/declarations?size=1000&type=DECLARATION_TYPE&bill-type=BILL_TYPE",
       "DECLARATION_TYPE, , /lookup/declarations?size=1000&type=DECLARATION_TYPE",
-      ", BILL_TYPE, /lookup/declarations?size=1000&billType=BILL_TYPE",
+      // A blank filter is left off rather than sent as an empty value.
+      "DECLARATION_TYPE, '', /lookup/declarations?size=1000&type=DECLARATION_TYPE",
+      "DECLARATION_TYPE, '   ', /lookup/declarations?size=1000&type=DECLARATION_TYPE",
+      "'', BILL_TYPE, /lookup/declarations?size=1000&bill-type=BILL_TYPE",
+      "'   ', BILL_TYPE, /lookup/declarations?size=1000&bill-type=BILL_TYPE",
+      "'', '', /lookup/declarations?size=1000",
+      ", BILL_TYPE, /lookup/declarations?size=1000&bill-type=BILL_TYPE",
       ", , /lookup/declarations?size=1000"
     })
     @DisplayName("getDeclarations parameterized test")
@@ -1523,7 +1529,7 @@ public class EbsApiClientTest {
 
     @ParameterizedTest
     @CsvSource({
-      "DECLARATION_TYPE, BILL_TYPE, /lookup/declarations?size=1000&type=DECLARATION_TYPE&billType=BILL_TYPE",
+      "DECLARATION_TYPE, BILL_TYPE, /lookup/declarations?size=1000&type=DECLARATION_TYPE&bill-type=BILL_TYPE",
       "DECLARATION_TYPE, , /lookup/declarations?size=1000&type=DECLARATION_TYPE"
     })
     @DisplayName("getDeclarations handles errors in a parameterized test")
